d1cd10cb2b77305af348b35f5af7abc527024a3b
slady <csl.bunny.2207@gmail.com>
2022-11-26 22:07:04 +0100

    template literal

--- assignments/javascript.js
+++ assignments/javascript.js
@@ -12,23 +12,42 @@ console.log(isIsland, population, country, language);
 const now = 2037;
 const ageSlady = now - 1994;
 const ageYuri = now - 1999;
-console.log(ageSlady, ageYuri);
+const averageAge = (ageSlady + ageYuri) / 2;
+// console.log(ageSlady, ageYuri, averageAge);
 
-const firstName = "Sladjana";
-const lastName = "Dejanovic";
-console.log(firstName + " " + lastName);
+// const firstName = "Sladjana";
+// const lastName = "Dejanovic";
+// console.log(firstName + " " + lastName);
 
 // ASSIGNMENT OPERATORS
 let x = 10 + 5; //x ce biti 15
 x += 10; // na vrednost x (koja je 15 zbog ovog iznad) dodaje se 10
 x *= 4; // vrednost x (koja je sad 25) mnozi se sa 4
 x++; //x = x + 1
-console.log(x);
+// console.log(x);
 
 // COMPARISON OPERATORS
-console.log(ageSlady > ageYuri);
-console.log(ageYuri >= 30);
+// console.log(ageSlady > ageYuri);
+// console.log(ageYuri >= 30);
 
 const isAge = ageYuri >= 30; //store this result if we need it later in code
 
-console.log(now - 1994 > now - 1999); //isto sto i ovo: console.log(ageSlady > ageYuri);, da ne mora da se racuna posebno (12, 13, 14 red), nego odmah ovde ovako
+// console.log(now - 1994 > now - 1999); //isto sto i ovo: console.log(ageSlady > ageYuri);, da ne mora da se racuna posebno (12, 13, 14 red), nego odmah ovde ovako
+
+//STRINGS AND ...
+const firstName = "Slady";
+const job = "bookbinder";
+const birthYear = 1994;
+const year = 2030;
+
+const slady =
+  "I'm " + firstName + ", a " + (year - birthYear) + " yaers old " + job;
+console.log(slady);
+
+//...TEMPLATE LITERALS
+const sladyNew = `I'm ${firstName}, a ${year - birthYear} year old ${job}`;
+console.log(sladyNew);
+//laksi nacin da se napise string. koristi se back tick, dugme iznad taba. variables se pisu ${ime var, ili func ili bilo sta}
+console.log(`String
+multiple
+lines`); //creating multi line string (enter...logicno)
