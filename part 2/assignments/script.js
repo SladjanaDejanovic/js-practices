1d5beeb635469e593bcfef7f7124c0a15e1a46df
slady <csl.bunny.2207@gmail.com>
2022-12-10 23:37:13 +0100

    function declaration and function expression

--- part 2/assignments/script.js
+++ part 2/assignments/script.js
@@ -6,3 +6,20 @@ const descSerbia = describeCountry("Serbia", 6.8, "Belgrade");
 const descBrazil = describeCountry("Brazil", 214, "Brasilia");
 const descKorea = describeCountry("South Korea", 51.74, "Seoul");
 console.log(descSerbia, descBrazil, descKorea);
+
+function percentageOfWorld1(population) {
+  return (population / 7900) * 100;
+}
+
+const serbia = percentageOfWorld1(6.8);
+const brazil = percentageOfWorld1(214);
+const southKorea = percentageOfWorld1(51.7);
+
+console.log(serbia, brazil, southKorea);
+
+const percentageOfWorld2 = function (population) {
+  return (population / 7900) * 100;
+};
+
+const serbia2 = percentageOfWorld2(6.8);
+console.log(serbia2);
--- part 2/functions/script.js
+++ part 2/functions/script.js
@@ -1,6 +1,6 @@
 "use strict";
 
-/////// STRICT MODE
+///////     STRICT MODE       /////////////
 
 //writing more secure code using strict mode:
 //it has to be first line of code, otherwise it won't work
@@ -19,7 +19,7 @@ if (hasDriversLicense) console.log("I can drive!");
 //const interface = "Audio";
 //const private = 234;
 
-///////  FUNCTIONS
+///////      FUNCTIONS       ////////////
 // - piece of code that we can reuse, contains several lines of code, can recieve data and return data back
 // function functionName(parameters, they are like variables that are speciffic only to this function, they will get defined once we called this function){code to execute}
 
@@ -40,9 +40,43 @@ fruitProcessor(5, 0);
 
 //if we want to use that value that was returned (in this case juice), we need to store it in a variable:
 const appleJuice = fruitProcessor(5, 0);
-console.log(appleJuice);
+//console.log(appleJuice);
 
 const appleOrangeJuice = fruitProcessor(2, 4);
-console.log(appleOrangeJuice);
+//console.log(appleOrangeJuice);
 
 //******** * keep your code DRY = Don't Repeat Yourself **********////////
+
+/////      DECLARATION VS EXPRESSION  ///////////////
+
+///       FUNCTION DECLARATION
+function calcAge1(birthYear) {
+  const age = 2037 - birthYear; // we calculate the age first
+  return age; //and then return the value, we take it out of the function using "return " keyword, so we can use it later
+}
+
+//could be simplified, if all we want to do is to return this value, we don't need to store it in a variable:
+
+function calcAge2(birthYear) {
+  //we're using birth year that we gonna recieve and we use that to calculate the age
+  //birthYear is an input
+  return 2037 - birthYear;
+}
+
+const age2 = calcAge2(1994);
+
+//the parameter is kind of placeholder in the function (birthYear) and the argument is then the actual value that we use to fill in that placeholder that is the parameter (1994).
+
+////     FUNCTION EXPRESSION
+
+// storing function in a variable (without giving a aname to function, this is annonymous function) and then that variable will be the function:
+const calcAge3 = function (birthYear) {
+  return 2037 - birthYear;
+};
+
+// expression produces value (this is expression:    function (birthYear) {return 2037 - birthYear;}   ), so weasign this value to this variable. this variable will hold function value basically
+
+const age3 = calcAge3(1994);
+console.log(age2, age3);
+
+//// we can call function declarationS BEFORE THEY ARE DEFINED IN A CODE. This isn't working with expressions
