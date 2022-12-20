68b30e77cf32398e026f7addc5fbf2931f7cc4a5
slady <csl.bunny.2207@gmail.com>
2022-12-20 22:19:59 +0100

    object methods

--- part 2/objects/script.js
+++ part 2/objects/script.js
@@ -13,26 +13,26 @@ order of elements in object doesn't matter at all
 
 //the easiest way of creating objects is with this {}, and it's called object literal syntax
 
-const slady = {
-  firstName: "Sladjana",
-  lastName: "Dejanovic",
-  age: 2022 - 1994,
-  job: "frontend dev",
-  friends: ["Yuri", "Kris", "Marija", "Marijana"],
-};
+// const slady = {
+//   firstName: "Sladjana",
+//   lastName: "Dejanovic",
+//   age: 2022 - 1994,
+//   job: "frontend dev",
+//   friends: ["Yuri", "Kris", "Marija", "Marijana"],
+// };
 
 //////// DOT vs BRACKET NOTATION
-console.log(slady);
+//console.log(slady);
 
 //getting a property from an object by dot notaion (using final name of a value):
-console.log(slady.lastName);
+//console.log(slady.lastName);
 
 //or with brackets (in [] can go any expression, so we can use computed propery values too):
-console.log(slady["lastName"]);
+//console.log(slady["lastName"]);
 
 const nameKey = "Name";
-console.log(slady["first" + nameKey]);
-console.log(slady["last" + nameKey]);
+//console.log(slady["first" + nameKey]);
+//console.log(slady["last" + nameKey]);
 
 // const interestedIn = prompt(
 //   "What do you want to know about Slady? Choose among firstName, lastName, age, job, and friends"
@@ -47,13 +47,27 @@ console.log(slady["last" + nameKey]);
 // }
 
 ////adding properties to the object:
-slady.location = "Serbia";
-slady["instagram"] = "waywardcherrypie";
-console.log(slady);
+//slady.location = "Serbia";
+//slady["instagram"] = "waywardcherrypie";
+//console.log(slady);
 
 //challenge: "Sladjana has 4 friends, and her best friend is called Yuri"
-console.log(
-  `${slady.firstName} has ${slady.friends.length} friends, and her best friend is called ${slady.friends[0]}.`
-);
+// console.log(
+//   `${slady.firstName} has ${slady.friends.length} friends, and her best friend is called ${slady.friends[0]}.`
+// );
 
 ////// OBJECT METHODS //////
+
+const slady = {
+  firstName: "Sladjana",
+  lastName: "Dejanovic",
+  birthYear: 1994,
+  job: "frontend dev",
+  friends: ["Yuri", "Kris", "Marija", "Marijana"],
+  hasDriversLicense: false,
+
+  calcAge: function (birthYear) {
+    return 2022 - birthYear;
+  }, // every function that is attached to an object is called method
+};
+console.log(slady.calcAge(1994));
