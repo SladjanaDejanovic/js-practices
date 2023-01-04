83f058ebe55fe8aada248845cb34573bba8592d2
slady <csl.bunny.2207@gmail.com>
2023-01-04 23:13:33 +0100

    guess the secret number

--- project 1 - Guess my number/script.js
+++ project 1 - Guess my number/script.js
@@ -2,7 +2,7 @@
 /*
 console.log(document.querySelector(".message").textContent);
 
-document.querySelector(".message").textContent = "Correct number";
+
 
 document.querySelector(".number").textContent = 13;
 document.querySelector(".score").textContent = 10;
@@ -10,7 +10,8 @@ document.querySelector(".score").textContent = 10;
 document.querySelector(".guess").value = 23; //bc .guess is input, so we can change its value
 */
 
-//HANDLING CLICK EVENTS
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector(".number").textContent = secretNumber;
 
 //Event here is "click", and we need to specify the reaction to the click event. We are doing that by defining a function which is called the event handler.
 //.addEventListener - 1st arguments is event. 2nd argument is event handler function
@@ -21,5 +22,11 @@ document.querySelector(".check").addEventListener("click", function () {
 
   if (!guess) {
     document.querySelector(".message").textContent = "⛔ No number!";
+  } else if (guess === secretNumber) {
+    document.querySelector(".message").textContent = "🎉 Correct number!";
+  } else if (guess > secretNumber) {
+    document.querySelector(".message").textContent = "📈 Too high!";
+  } else if (guess < secretNumber) {
+    document.querySelector(".message").textContent = "📉 Too low!";
   }
 });
