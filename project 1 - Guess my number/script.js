e1d8e51d4fc88f88bcbe6638417310340ba7b7cf
slady <csl.bunny.2207@gmail.com>
2023-01-06 15:50:49 +0100

    again button - reseting the game

--- project 1 - Guess my number/script.js
+++ project 1 - Guess my number/script.js
@@ -7,10 +7,9 @@ document.querySelector(".number").textContent = 13;
 document.querySelector(".guess").value = 23; //bc .guess is input, so we can change its value
 */
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20; //it's better to store value in variable in code, than to have it only on DOM
 //this score variable could be called state variable, bc it's a part of so-called aplication state(all data relevant to the aplication)
-document.querySelector(".number").textContent = secretNumber;
 
 //Event here is "click", and we need to specify the reaction to the click event. We are doing that by defining a function which is called the event handler.
 //.addEventListener - 1st arguments is event. 2nd argument is event handler function
@@ -26,6 +25,7 @@ document.querySelector(".check").addEventListener("click", function () {
     //When player wins
   } else if (guess === secretNumber) {
     document.querySelector(".message").textContent = "🎉 Correct number!";
+    document.querySelector(".number").textContent = secretNumber;
 
     document.querySelector("body").style.backgroundColor = "#60b347"; //in css it's background-color, but in js it's with cammel case writing, so it then becomes backgroundColor. this is the case for all properties that have 2 or more words in their name
     document.querySelector(".number").style.width = "30rem"; //whenever we're manipulating a style we always specify a string
@@ -52,4 +52,16 @@ document.querySelector(".check").addEventListener("click", function () {
       document.querySelector(".score").textContent = 0;
     }
   }
+});
+
+document.querySelector(".again").addEventListener("click", function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector(".message").textContent = "Start guessing...";
+  document.querySelector(".score").textContent = score;
+  document.querySelector(".number").textContent = "?";
+  document.querySelector(".guess").value = "";
+
+  document.querySelector(".number").style.width = "15rem";
+  document.querySelector("body").style.backgroundColor = "#222";
 });
