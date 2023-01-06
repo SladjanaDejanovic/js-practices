18f2c80c79a2050286f1b8ad01344b6644cdc787
slady <csl.bunny.2207@gmail.com>
2023-01-06 21:07:58 +0100

    active player and score

--- project 3 - Pig game/script.js
+++ project 3 - Pig game/script.js
@@ -1,10 +1,46 @@
 "use strict";
 
-//Selecting elements
+// Selecting elements
+const player0El = document.querySelector(".player--0");
+const player1El = document.querySelector(".player--1");
 const score0El = document.querySelector("#score--0");
-const score1El = document.getElementById("score"); //different way of selecting elemnts, and this works faster than querySelector
-const diceEl = document.querySelector(".dice");
+const score1El = document.getElementById("score--1"); //different way of selecting elements, and this works faster than querySelector
+const current0El = document.getElementById("current--0");
+const current1El = document.getElementById("current--1");
 
+const diceEl = document.querySelector(".dice");
+const btnNew = document.querySelector(".btn--new");
+const btnHold = document.querySelector(".btn--hold");
+const btnRoll = document.querySelector(".btn--roll");
+
+// Starting conditions
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add("hidden");
+const scores = [0, 0]; //score of player 1 would be in index 0, player 2 in index 1
+let currentScore = 0;
+let activePlayer = 0;
+
+// Rolling dice functionality
+btnRoll.addEventListener("click", function () {
+  //1.Generating random dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  console.log(dice);
+  //2.Display dice
+  diceEl.classList.remove("hidden");
+  diceEl.src = `dice-${dice}.png`;
+  //3.Check for rolled 1: if true, switch player
+  if (dice !== 1) {
+    //Add dice to the current score
+    currentScore += dice;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+  } else {
+    //Switch to next player
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle("player--active");
+    player1El.classList.toggle("player--active");
+  }
+});
