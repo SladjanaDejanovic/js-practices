7143f557cbf0942ff0acf2b83d753b25f9ad21fb
slady <csl.bunny.2207@gmail.com>
2023-01-08 22:48:56 +0100

    scoping practise

---
+++ behind the scenes/script.js
@@ -0,0 +1,12 @@
+"use strict";
+
+/// Scope
+
+function calcAge(birthYear) {
+  const age = 2022 - birthYear;
+  console.log(firstName);
+  return age;
+}
+
+const firstName = "Slady";
+calcAge(1994);
