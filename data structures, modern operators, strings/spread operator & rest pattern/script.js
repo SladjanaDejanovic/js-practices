78dbcece073a558f63f294ec02a49f0523240b50
slady <csl.bunny.2207@gmail.com>
2023-01-14 17:41:12 +0100

    rest arguments

--- data structures, modern operators, strings/spread operator & rest pattern/script.js
+++ data structures, modern operators, strings/spread operator & rest pattern/script.js
@@ -48,6 +48,14 @@ const restaurant = {
       `Here is your delicious pasta with ${ing1}, ${ing2}, and ${ing3}`
     );
   },
+
+  orderPizza: function (
+    mainIngredient,
+    ...otherIngredients // has to have at least one ingredient, and others are optional
+  ) {
+    console.log(mainIngredient);
+    console.log(otherIngredients);
+  },
 };
 
 //// Spread operator ////////////
@@ -123,3 +131,25 @@ console.log(pizza, risotto, otherFood); // this otherFood only includes elements
 
 const { sat, ...weekdays } = restaurant.openingHours;
 console.log(weekdays);
+
+// functions
+// Rest parameters
+
+const add = function (...numbers) {
+  // taking multiple numbers and then packs them into one array
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) sum += numbers[i];
+  console.log(sum);
+};
+add(2, 3);
+add(5, 3, 7, 2);
+add(8, 2, 5, 3, 2, 1, 4);
+
+const x = [23, 5, 7];
+add(...x);
+
+restaurant.orderPizza("mushrooms", "cheese", "olives", "eggs");
+restaurant.orderPizza("mushrooms");
+
+/// we use REST when we have to write multiple variables separated by commas
+// SPREAD - when we have to use several values separated by commas
