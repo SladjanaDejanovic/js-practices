ffc7528cb0f8e27f13d1088fc0f8e29b6209b1e2
slady <csl.bunny.2207@gmail.com>
2023-01-15 23:03:35 +0100

    code challenge

---
+++ behind the scenes/challenge 1/script.js
@@ -0,0 +1,19 @@
+"use strict";
+/*
+1. Create one player array for each team (variables players1 and players2)
+
+2. the first player in any array is the goalkeeper and the others are field players. for bayern munich (team1) create one variable ("gk") with the goalkeeper's name, and one array ("fieldPlayers") with all the remaining 10 field player
+
+3. create an array "allPlayers" containing all players of both teams (22 players)
+
+4. during the game, bayern munich (team 1) used 3 substitute players. so create a new array ("playersFinal") containing all thr original team1 players plus "Thiago", "Coutinho" and "Perisic"
+
+5. based on the game.odds object, create one variable for each odd (called "team1", "draw", and "team2")
+
+6. write a function ("printGoals") that receives an arbitrary number of player names (NOT an array) and prints each of them to the console, along with the number of goals who were scored (number of player names passed in)
+
+7. the team with the lower odd is more likely to win. print to the console which team is more likely to win, WITHOUT using if/else statement or the ternary operator
+
+TEST DATA FOR 6: use players "Davies", "Mullier", "Lewandowski" and "Kimmich". then call the function again with players from game.scored
+
+*/
