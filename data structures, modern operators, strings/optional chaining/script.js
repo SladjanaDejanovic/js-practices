6dc4a67c8be690107945bf7a02999b7cbdf58be7
slady <csl.bunny.2207@gmail.com>
2023-01-19 23:51:21 +0100

    ?. on methods and arrays

--- data structures, modern operators, strings/optional chaining/script.js
+++ data structures, modern operators, strings/optional chaining/script.js
@@ -80,3 +80,12 @@ for (const day of days) {
 
   console.log(`On ${day}, we open at ${open}`);
 }
+
+// Methods (checking if method exists before calling it):
+console.log(restaurant.order?.(0, 1) ?? "Method does not exist");
+console.log(restaurant.orderRisotto?.(0, 1) ?? "Method does not exist");
+
+// Arrays (checking if it's empty)
+const users = [{ name: "Slady", email: "hello@slady.com" }];
+// const users = [];
+console.log(users[0]?.name ?? "User array empty");
