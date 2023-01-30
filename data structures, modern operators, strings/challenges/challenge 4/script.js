5eb67d398fa224328bda0578f611f35dfa3b832c
slady <csl.bunny.2207@gmail.com>
2023-01-30 21:46:35 +0100

    underscore to cammelCase

--- data structures, modern operators, strings/challenges/challenge 4/script.js
+++ data structures, modern operators, strings/challenges/challenge 4/script.js
@@ -12,10 +12,10 @@ Some_Variable
 delayed_departure
 
 SHOULD PRODUCE THIS OUTPUT (5 separate console.log outputs)
-UnderscoreCase      ✅
+underscoreCase      ✅
 firstName           ✅✅
 someVariable        ✅✅✅
-CalculateAge        ✅✅✅✅
+calculateAge        ✅✅✅✅
 delayedDeparture    ✅✅✅✅✅
 
 Hint 1: Remember which character defines a new line in the text area (/n)
@@ -23,51 +23,37 @@ Hint 2: The solution only needs to work for a variable made out of 2 words, like
 hint 3: Start without wottying about ✅. Tackle that only after you have variable name conversion working
 
 Afterwards, test with your own data
-
-
 */
 
 document.body.append(document.createElement("textarea"));
 document.body.append(document.createElement("button"));
-const text = document.querySelector("textarea").value;
 
-// to lower case
-// const bla = "BLA_bLA";
-// const lowerBla = bla.toLowerCase();
-// console.log(lowerBla);
-// // remove _
-// const together = lowerBla.split("_");
-// console.log(together);
-// const first = together[0];
-// console.log(first);
-//add capital letter on second
-// const capital = together[1].toUpperCase();
-// console.log(capital);
-// const secondCapital = capital[0] + capital.slice(1).toLowerCase();
-// console.log(secondCapital);
-// const newArr = [];
-// newArr.push(first);
-// newArr.push(secondCapital);
-// console.log(newArr);
-// const all = newArr.join("");
-// console.log(all);
+document.querySelector("button").addEventListener("click", function () {
+  const text = document.querySelector("textarea").value;
+  const rows = text.split("\n");
+  //my try:
+  // const cammelCase = [];
+  // for (let i = 0; i < rows.length; i++) {
+  //   const lower = rows[i].toLowerCase();
+  //   const split = lower.split("_");
+  //   const first = split[0];
+  //   const second = split[1];
+  //   const secondUpper = second.toUpperCase();
+  //   const secondCapital = secondUpper[0] + secondUpper.slice(1).toLowerCase();
+  //   cammelCase.push(first);
+  //   cammelCase.push(secondCapital);
+  //   console.log(cammelCase);
+  //   cammelCase.join("");
+  // }
 
-const cammelCase = [];
+  for (const [i, row] of rows.entries()) {
+    const [first, second] = row.toLowerCase().trim().split("_");
+    const output = `${first}${second.replace(
+      second[0],
+      second[0].toUpperCase()
+    )}`;
+    // console.log(output.padEnd(20)); // if we wanna add empty spaces, we can just put the number for how long we want the string, and it will add spaces at the end
 
-const toCammelCase = function (text) {
-  // const arr = strings.split("/n");
-  const lower = text.toLowerCase();
-  const split = lower.split("_");
-
-  const first = split[0];
-  const second = split[1];
-
-  const secondUpper = second.toUpperCase();
-  const secondCapital = secondUpper[0] + secondUpper.slice(1).toLowerCase();
-
-  cammelCase.push(first);
-  cammelCase.push(secondCapital);
-
-  return console.log(cammelCase.join(""));
-};
-toCammelCase("undersocre_case");
+    console.log(`${output.padEnd(20)}${"✅".repeat(i + 1)}`);
+  }
+});
