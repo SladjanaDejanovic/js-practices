36becf8ae8869e802b0a142b1c956fa494ac1263
slady <csl.bunny.2207@gmail.com>
2023-02-08 23:04:40 +0100

    passing value and passing reference

--- functions/script.js
+++ functions/script.js
@@ -27,3 +27,29 @@ createBooking("LH123", 2);
 createBooking("LH123", 5);
 //if we wanna skip second parameter, only use flight number and price:
 createBooking("LH123", undefined, 5); // this is the same as not declaring second argument, so in console it will have default value
+
+const flight = "LH234";
+const slady = {
+  name: "Sladjana Dejanovic",
+  passport: 2345689405,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = "LH999";
+  passenger.name = "Ms." + passenger.name;
+
+  if (passenger.passport === 2345689405) {
+    alert("Check in");
+  } else {
+    alert("Wrong passport!");
+  }
+};
+
+checkIn(flight, slady);
+console.log(flight);
+console.log(slady);
+
+// passing primitive type in a func is the same as crating a copy outside of the function, the value is simply copied
+// when passing a object, it's like copying that object, so whatever we change in a copy will also change in the original
+
+// passing by value and passing by reference - in js there's no passing by reference(we pass the refrence to the function but not pass by refrence)
