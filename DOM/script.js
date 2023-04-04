bb65e11d997a9da17cad3533bde8b1559c395653
slady <csl.bunny.2207@gmail.com>
2023-04-04 20:47:57 +0200

    slider

--- DOM/script.js
+++ DOM/script.js
@@ -281,3 +281,52 @@ const obsOptions = {
 
 const observer = new IntersectionObserver(obsCallback, obsOptions);
 observer.observe(section1);
+
+////// Slider
+
+const slides = document.querySelectorAll('.slide');
+const btnLeft = document.querySelector('.slider__btn--left');
+const btnRight = document.querySelector('.slider__btn--right');
+
+let curSlide = 0;
+const maxSlide = slides.length;
+
+// const slider = document.querySelector('.slider');
+// slider.style.transform = 'scale(0.5)';
+// slider.style.overflow = 'visible';
+
+// 0%, 100%, 200%, 300% - for 4 slides
+
+const goToSlide = function (slide) {
+  slides.forEach(
+    (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
+  );
+};
+goToSlide(0);
+
+// Next slide
+const nextSlide = function () {
+  if (curSlide === maxSlide - 1) {
+    curSlide = 0;
+  } else {
+    curSlide++;
+  }
+
+  // curSlide = 1: -100$, 0%, 100%, 200%
+  goToSlide(curSlide);
+};
+
+btnRight.addEventListener('click', nextSlide);
+
+// Previous slide
+const prevSlide = function () {
+  if (curSlide === 0) {
+    curSlide = maxSlide - 1;
+  } else {
+    curSlide--;
+  }
+
+  goToSlide(curSlide);
+};
+
+btnLeft.addEventListener('click', prevSlide);
