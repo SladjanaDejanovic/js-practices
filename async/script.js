0ff26e63543841e79f26ceff8d09b3ef9396f0d4
slady <csl.bunny.2207@gmail.com>
2023-06-03 22:31:42 +0200

    ajax call

--- async/script.js
+++ async/script.js
@@ -8,24 +8,8 @@ const countriesContainer = document.querySelector('.countries');
 //////////////////////////////
 // Doing AJAX calls with XMLHttp request (old school way)
 
-const getCountryData = function (country) {
-  const request = new XMLHttpRequest();
-
-  // on github there is a huge repository of public APIs (here we're using REST countries; any API i want to use from here should have CORS(cross origine resource sharing) set to yes/unknown)
-  request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
-  request.send();
-
-  request.addEventListener('load', function () {
-    const [data] = JSON.parse(this.responseText);
-    console.log(data);
-
-    // fixed bug that appears bc code from course isn not working today
-    const languages = Object.values(data.languages);
-    const currencies = Object.values(data.currencies);
-    const name = Object.values(data.name);
-    const flags = Object.values(data.flags);
-
-    const html = `<article class="country">
+const renderCountry = function (data) {
+  const html = `<article class="country">
   <img class="country__img" src="${flags[0]}" />
   <div class="country__data">
     <h3 class="country__name">${name[0]}</h3>
@@ -38,12 +22,33 @@ const getCountryData = function (country) {
   </div>
 </article>`;
 
-    countriesContainer.insertAdjacentHTML('beforeend', html);
-    countriesContainer.style.opacity = 1;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+const getCountryAndNeighbour = function (country) {
+  // AJAX call country 1
+  const request = new XMLHttpRequest();
+
+  // on github there is a huge repository of public APIs (here we're using REST countries; any API i want to use from here should have CORS(cross origine resource sharing) set to yes/unknown)
+  request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+  request.send();
+
+  renderCountry(data);
+
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+
+    // fixed bug that appears bc code from course isn not working today
+    const languages = Object.values(data.languages);
+    const currencies = Object.values(data.currencies);
+    const name = Object.values(data.name);
+    const flags = Object.values(data.flags);
   });
 };
 
-getCountryData('serbia');
-getCountryData('brazil');
-getCountryData('japan');
-getCountryData('korea');
+getCountryDataAndNEighbour('serbia');
+getCountryDataAndNEighbour('brazil');
+getCountryDataAndNEighbour('japan');
+getCountryDataAndNEighbour('korea');
