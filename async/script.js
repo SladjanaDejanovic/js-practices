cfc6658714aabce9f08000998d13fe9f74d64635
slady <csl.bunny.2207@gmail.com>
2023-06-09 22:34:18 +0200

    fixed bug for flags and neighbour

--- async/script.js
+++ async/script.js
@@ -113,15 +113,18 @@ const getCountryData = function (country) {
   // country 1
   fetch(`https://restcountries.com/v3.1/name/${country}`)
     .then(response => response.json())
-    // Chaining promises
     .then(data => {
+      console.log(data);
       renderCountry(data[0]);
       const neighbour = data[0].borders[0];
+
+      if (!neighbour) return;
+
       // country 2
       return fetch(`https://restcountries.com/v3.1/alpha/${neighbour}`);
     })
     .then(response => response.json())
-    .then(data => renderCountry(data, 'neighbour'));
+    .then(data => renderCountry(data[0], 'neighbour'));
 };
 
 getCountryData('serbia');
