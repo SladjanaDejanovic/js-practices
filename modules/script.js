10b5b9a33c75aa09b710b9e7f12ace8d56d08feb
slady <csl.bunny.2207@gmail.com>
2023-07-30 22:41:45 +0200

    exporting and importing in modules

---
+++ modules/script.js
@@ -0,0 +1,31 @@
+// Importing module
+
+// import { addToCart } from './shoppingCart.js'; // string with a location of the module
+// or importing multiple at once:
+// import { addToCart, totalPrice as price, tq } from './shoppingCart.js'; // we can rename imports: totalPrice as price
+// addToCart('bread', 5);
+// console.log(price, tq);
+
+console.log('Importing module');
+
+// importing all the exports at once:
+// creating an object containg everything that is exported in another module
+// import * as ShoppingCart from './shoppingCart.js';
+// this module is now basically exporting a public API, just like a class (with methods and promerties)
+
+// to use something from here we have to take it out from this object
+// ShoppingCart.addToCart('bread', 5);
+// console.log(ShoppingCart.totalPrice);
+
+//importing default export (giving it a name we want, without {} like with a named export/import)
+import add from './shoppingCart.js';
+add('pizza', 2);
+
+// default and named exports can be imported together (but in practice we never mix these two):
+// import add, { addToCart, totalPrice as price, tq } from './shoppingCart.js';
+// console.log(price);
+
+import { cart } from './shoppingCart.js';
+add('bread', 3);
+add('apples', 6);
+console.log(cart); // import is a live connection, not simply a copy, they point at the same place in memory (cart was empty array as export from another module, but after calling add() which pushes objects to array, now cart looks like this)
---
+++ modules/shoppingCart.js
@@ -0,0 +1,28 @@
+// Exporting module
+console.log('Exporting module');
+
+// variables decalred in module are module scoped, private to this module
+const shippingCost = 10;
+export const cart = [];
+
+// we make them accessable by using exports: named exportes and default exports
+// exports always need to happen in top level code (it can't be inside a function or if statement for example)
+
+// named export
+export const addToCart = function (product, quantity) {
+  cart.push({ product, quantity });
+  console.log(`${quantity} ${product} added to cart`);
+};
+
+const totalPrice = 237;
+const totalQuantity = 23;
+
+// exporting multiple at once
+export { totalPrice, totalQuantity as tq }; // we can also rename exports here immediatelly
+
+// default export - used when we want to export only 1 thing per module; we are exporting the value itself, not the variable (not the name of the function). so when we're importing default we can give it any name
+
+export default function (product, quantity) {
+  cart.push({ product, quantity });
+  console.log(`${quantity} ${product} added to cart`);
+}
