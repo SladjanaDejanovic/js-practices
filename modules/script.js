6583f99d61b37d1f4f7b3cb5c1091ad403f6b3fd
slady <csl.bunny.2207@gmail.com>
2023-08-01 20:36:27 +0200

    module patern

--- modules/script.js
+++ modules/script.js
@@ -51,3 +51,32 @@ const lastPost = getLastPost();
 
 const lastPost2 = await getLastPost();
 console.log(lastPost2);
+
+//// Module patern (way of using modules before) - making IIFE (immediatelly invoked function expression)
+const ShoppingCart2 = (function () {
+  const cart = [];
+  const shippingCost = 10;
+  const totalPrice = 237;
+  const totalQuantity = 23;
+
+  const addToCart = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(`${quantity} ${product} added to cart`);
+  };
+
+  const orderStock = function (product, quantity) {
+    console.log(`${quantity} ${product} ordered from supplier`);
+  };
+
+  return {
+    addToCart,
+    cart,
+    totalPrice,
+    totalQuantity,
+  };
+})();
+
+ShoppingCart2.addToCart('apple', 4);
+ShoppingCart2.addToCart('pizza', 2);
+console.log(ShoppingCart2);
+console.log(ShoppingCart2.shippingCost);
