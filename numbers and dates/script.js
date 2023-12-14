db72e43628bf05e38b674dd52731b06c3f526bba
slady <csl.bunny.2207@gmail.com>
2023-12-14 20:55:52 +0100

    study

--- numbers and dates/script.js
+++ numbers and dates/script.js
@@ -64,9 +64,9 @@ console.log(25 ** (1 / 2));
 console.log(8 ** (1 / 3));
 
 // maximum value
-console.log(Math.max(5, 18, 23, 11, 2));
+// console.log(Math.max(5, 18, 23, 11, 2));
 // it odeas type coersion
-console.log(Math.max(5, 18, '23', 11, 2));
+// console.log(Math.max(5, 18, '23', 11, 2));
 // but no parsing
 // console.log(Math.max(5, 18, '23px', 11, 2));
 
