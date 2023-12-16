69d372ff8057d771479e1b9e518cb294969a99c8
slady <csl.bunny.2207@gmail.com>
2023-12-16 21:51:53 +0100

    math

--- numbers and dates/script.js
+++ numbers and dates/script.js
@@ -61,7 +61,7 @@ console.log(Math.sqrt(25));
 // console.log(25 ** (1 / 2));
 
 //cubic root
-console.log(8 ** (1 / 3));
+// console.log(8 ** (1 / 3));
 
 // maximum value
 // console.log(Math.max(5, 18, 23, 11, 2));
